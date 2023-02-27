cf0dc290b131cc31b69a959392cd6783b698d118
Shawn.S <93225931+zikangS@users.noreply.github.com>
2023-02-27 21:32:47 +0100

    Add files via upload

---
+++ frequency_voice_authentication_light/frequency_voice_authentication_light.ino
@@ -0,0 +1,92 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <iostream>
+#include <string>
+
+//---------------------------------------------------------------------------------------
+AudioInputI2S            i2s1;           
+AudioFilterStateVariable filter1;       
+AudioAmplifier           amp1;           
+AudioAnalyzeNoteFrequency notefreq;
+AudioControlSGTL5000     audioShield;      
+//---------------------------------------------------------------------------------------
+AudioConnection          patchCord1(i2s1, 0, filter1, 0);
+AudioConnection          patchCord2(filter1, 2, amp1, 0);
+AudioConnection          patchCord3(amp1, notefreq);
+//---------------------------------------------------------------------------------------
+const int ledPin =  LED_BUILTIN;
+int ledState = LOW;
+
+void setup() {
+    Serial.begin(9600);
+    AudioMemory(50);
+    audioShield.enable();
+    audioShield.inputSelect(AUDIO_INPUT_MIC);
+    audioShield.micGain(10); // in dB
+    audioShield.volume(0.5);
+    notefreq.begin(.15);
+    filter1.frequency(30); // filter out DC & extremely low frequencies
+    amp1.gain(8.5);        // amplify sign to useful range
+    pinMode(ledPin, OUTPUT);
+}
+
+std::string str ("");
+bool detected=false;
+
+void loop() {
+  if (notefreq.available()) {
+    float frequency = notefreq.read();
+    Serial.print("Frequency: ");
+    Serial.println(frequency);
+    //Serial.println(detected);
+    if (detected == false){
+      if (str.length()<3){
+         if (frequency>100 && frequency<125){
+          str+="b";
+          }
+         else if (frequency>=125 && frequency<145){
+          str+="c";
+          }
+         else{
+          str="";
+          }
+      }
+      else{
+        str.erase(0,1);
+         if (frequency>100 && frequency<125){
+          str+="b";
+          }
+          else if (frequency>=125 && frequency<145){
+          str+="c";
+          }
+         else{
+          str="";
+          }
+       }
+       if (str=="ccb"){
+        Serial.println("Hello");
+        detected=true;
+        } 
+       else{
+        }
+    }
+    else{
+      if (frequency>3000 && frequency<3600){
+        Serial.println("Switch");
+        detected=false;
+        // if the LED is off turn it on and vice-versa:
+        if (ledState == LOW) {
+          ledState = HIGH;
+        } else {
+          ledState = LOW;
+        }
+        digitalWrite(ledPin, ledState);
+      }
+    }
+  }
+  
+  //delay(150);
+}
